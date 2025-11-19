28d19e3f59451e83dba202fb549df7a4d2fe20c2
Gustavo Borges <gustavo@gbsoft.net>
2025-11-19 15:52:40 -0300

    add graceful shutdown

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/labstack/echo/v4"
 )
@@ -10,9 +15,24 @@ func main() {
 	e := echo.New()
 	e.GET("/hello", handlerHello)
 
-	e.Logger.Fatal(e.Start(":1323"))
+	terminationCtx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	go func() {
+		if err := e.Start(":1323"); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("shutting down the server...")
+		}
+	}()
+	<-terminationCtx.Done()
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(shutdownCtx); err != nil {
+		e.Logger.Fatal(err)
+	}
 }
 
 func handlerHello(c echo.Context) error {
+	time.Sleep(3 * time.Second) // Test graceful shutdown
 	return c.String(http.StatusOK, "Hello, SREday!")
 }
