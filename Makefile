b66b880bf26930e16658f16ded4e9370cffb7735
Gustavo Borges <gustavo@gbsoft.net>
2025-11-24 08:57:10 -0300

    add structured logs

---
+++ Makefile
@@ -0,0 +1,9 @@
+
+dev:
+	@air
+
+run:
+	@go run main.go
+
+test:
+	@go run test -v ./...
--- main.go
+++ main.go
@@ -8,10 +8,29 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Info().Msg("No .env file found, using environment variables")
+	}
+	logLevel := os.Getenv("LOG_LEVEL")
+	switch strings.ToUpper(logLevel) {
+	case "ERROR":
+		zerolog.SetGlobalLevel(zerolog.ErrorLevel)
+	case "DEBUG":
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	default:
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+
+	}
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+
 	e := echo.New()
 	e.GET("/health", func(c echo.Context) error {
 		// Validate required connections...
@@ -24,16 +43,18 @@ func main() {
 
 	go func() {
 		if err := e.Start(":1323"); err != nil && err != http.ErrServerClosed {
-			e.Logger.Fatal("shutting down the server...")
+			log.Fatal().Msg("shutting down the server...")
 		}
 	}()
 	<-terminationCtx.Done()
 
 	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
+
 	if err := e.Shutdown(shutdownCtx); err != nil {
-		e.Logger.Fatal(err)
+		log.Fatal().Msg("shutting down the server...")
 	}
+	log.Info().Msg("Server shutdown successfully.")
 }
 
 func handlerHello(c echo.Context) error {
